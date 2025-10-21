c0db9f4ab4f0161a07ae6c5c77428c7a9108f603
Joseph King <joking777@gmail.com>
2025-10-21 16:17:03 -0400

    added custom weather block

--- src/BlockNoteEditor.jsx
+++ src/BlockNoteEditor.jsx
@@ -1,14 +1,42 @@
 import React from "react";
-import { useCreateBlockNote } from "@blocknote/react";
+import { BlockNoteSchema, filterSuggestionItems } from "@blocknote/core";
+import {
+  getDefaultReactSlashMenuItems,
+  SuggestionMenuController,
+  useCreateBlockNote,
+} from "@blocknote/react";
 import { BlockNoteView } from "@blocknote/mantine";
+import { createWeather, insertWeatherItem } from "./blocks/WeatherBlock";
 import "@blocknote/mantine/style.css"; // Import the stylesheet
 
+// const schema = BlockNoteSchema.create().extend({
+//   blockSpecs: {
+//     weather: createWeather(),
+//   },
+// });
+
+// const getCustomSlashMenuItems = (
+//   editor,
+// ) => [
+//   ...getDefaultReactSlashMenuItems(editor),
+//   insertWeatherItem(editor),
+// ];
+
 function BlockNoteEditor() {
+
   // Create a new editor instance
   const editor = useCreateBlockNote();
-
+  
   // Render the editor
-  return <BlockNoteView editor={editor} />;
+  return <BlockNoteView editor={editor} />
+  //   <SuggestionMenuController
+  //       triggerCharacter={"/"}
+  //       // Replaces the default Slash Menu items with our custom ones.
+  //       getItems={async (query) =>
+  //         filterSuggestionItems(getCustomSlashMenuItems(editor), query)
+  //       }
+  //     />
+  // </BlockNoteView>;
 }
 
 export default BlockNoteEditor;
---
+++ src/blocks/WeatherBlock.jsx
@@ -0,0 +1,37 @@
+import { insertOrUpdateBlock } from "@blocknote/core";
+import { createReactBlockSpec } from "@blocknote/react"
+import { HiOutlineSun } from "react-icons/hi";
+
+// https://www.blocknotejs.org/docs/features/custom-schemas/custom-blocks
+// https://www.blocknotejs.org/docs/react/components/suggestion-menus#changing-slash-menu-items
+
+export const insertWeatherItem = (editor) => ({
+  title: "Insert Weather",
+  onItemClick: () =>
+    insertOrUpdateBlock(editor, {
+      type: "weather",
+      content: [],
+      props: {forcast: "Sunny"} 
+    }),
+  aliases: ["weather", "rain", "snow", "sun", "clouds"],
+  group: "Other",
+  icon: <HiOutlineSun size={18} />,
+  subtext: "Used to insert a block with 'weather' below.",
+});
+
+export const createWeather = createReactBlockSpec(
+  {
+    type: "weather",
+    content: "inline",
+    propSchema: {
+      forecast: { default: "Rainy", values: ["Rainy", "Sunny", "Cloudy", "Snowy"] },
+    }
+  },
+  {
+    render: (props) => {
+      return <div className="weather-block">{props.block.props.forecast} weather in the forecast!</div>
+    }
+  });
+
+
+
